4f33dd0a87210260a9fa8ff4d0400fbcd0a0930d
AlexP <alexperepelyuk@gmail.com>
2025-02-02 17:51:37 +0100

    hw done

---
+++ src/js/2-form.js
@@ -0,0 +1,36 @@
+const form = document.querySelector('.feedback-form');
+const formData = { email: '', message: '' };
+const localStorageKey = 'feedback-form-state';
+
+form.addEventListener('input', event => {
+  if (event.target.name === 'email' || event.target.name === 'message') {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(localStorageKey, JSON.stringify(formData));
+  }
+});
+
+window.addEventListener('load', () => {
+  const savedFormData = localStorage.getItem(localStorageKey);
+
+  if (savedFormData) {
+    const parsedFormData = JSON.parse(savedFormData);
+    document.querySelector('input[name="email"]').value = parsedFormData.email;
+    document.querySelector('textarea[name="message"]').value =
+      parsedFormData.message;
+  }
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+  } else {
+    console.log(formData);
+    localStorage.removeItem(localStorageKey);
+    formData.email = '';
+    formData.message = '';
+    document.querySelector('input[name="email"]').value = '';
+    document.querySelector('textarea[name="message"]').value = '';
+  }
+});
